e48e59f6344c6d39ad6ddc43456560aba741b7d3
nestor <nmoreno@overcom.net>
2018-03-22 10:53:02 -0400

    Se reajusto la vista de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Banana
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('countries.index') }}">Countries</a>
+                    {{--<a href="{{ route('states.index') }}">States</a>--}}
+                    <a href="{{ route('cities.index') }}">Cities</a>
                 </div>
             </div>
         </div>
